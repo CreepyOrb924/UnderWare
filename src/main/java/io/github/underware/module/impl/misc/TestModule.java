ae3abb3859e0f22a77419983a5e924baee165c2b
CreepyOrb924 <41650640+CreepyOrb924@users.noreply.github.com>
2022-04-13 16:58:35 -0400

    Fix migrate chat lister to forge's built in event

--- src/main/java/io/github/underware/module/impl/misc/TestModule.java
+++ src/main/java/io/github/underware/module/impl/misc/TestModule.java
@@ -1,11 +1,11 @@
 package io.github.underware.module.impl.misc;
 
 import io.github.underware.UnderWare;
-import io.github.underware.event.PlayerSendChatMessageEvent;
 import io.github.underware.module.Category;
 import io.github.underware.module.ModuleBase;
 import io.github.underware.module.setting.impl.BooleanSetting;
 import io.github.underware.module.setting.impl.StringSetting;
+import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.input.Keyboard;
 
@@ -32,7 +32,7 @@ public class TestModule extends ModuleBase {
     }
 
     @SubscribeEvent
-    public void onPlayerSendChatMessage(PlayerSendChatMessageEvent event) {
+    public void onClientChat(ClientChatEvent event) {
         if (event.getMessage().startsWith(",")) {
             event.setMessage("Among us.");
         }
